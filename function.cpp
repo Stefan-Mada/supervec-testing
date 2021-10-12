4b37b7ff2d03046a3393d67690963889f7b36e87
Stefan-Mada <Stefan.Mada@utah.edu>
2021-10-12 16:16:33 -0600

    New bugs

--- function.cpp
+++ function.cpp
@@ -3,8 +3,8 @@
 int function(int val1, int val2){
 	if(val2 == 1)
 		return val1;
-//	else if (val2 < 1)
-//		return 0;
+	else if (val2 < 1)
+		return 0;
 
 	return val1 * function(val1, val2 - 1);
 }
