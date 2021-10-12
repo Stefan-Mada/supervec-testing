d05540bd021a535998b71c91565d9066de12d021
Stefan-Mada <Stefan.Mada@utah.edu>
2021-10-12 15:26:50 -0600

    New error found

--- function.cpp
+++ function.cpp
@@ -1,9 +1,10 @@
 #include "function.hpp"
 
-__m256i function(__m256i vec1, int val, int val2){
-	int val3 = val + val2;
-	val3 *= val2;
-	__m256i valVec = _mm256_set1_epi64x(val3);
-	__m256i returnvec = _mm256_cmpgt_epi64(valVec, vec1);
-	return returnvec;
+int function(int val1, int val2){
+	if(val2 == 1)
+		return val1;
+//	else if (val2 < 1)
+//		return 0;
+
+	return val1 * function(val1, val2 - 1);
 }
