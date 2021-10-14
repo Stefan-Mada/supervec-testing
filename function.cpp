34c53af2ef631d3cb16866eb4a3a0cc3c88be339
Stefan-Mada <Stefan.Mada@utah.edu>
2021-10-14 15:53:30 -0600

    Issue with constant synthesizer

--- function.cpp
+++ function.cpp
@@ -1,7 +1,26 @@
 #include "function.hpp"
 
-int* function(int val)
+void function(unsigned char vin[], unsigned char vout[])
 {
-	int* arr = new int[10];
-	return arr;
+	constexpr int height = 800;
+	constexpr int width = 600;
+
+	int currentIndex, avgColor, redColor, greenColor, blueColor;
+	
+	for(int i = 0; i < height; ++i)
+	{
+		for(int j = 0; j < width; ++j)
+		{
+			currentIndex = (i * width + j) * 3;
+
+			redColor = vin[currentIndex];
+			greenColor = vin[currentIndex + 1];
+			blueColor = vin[currentIndex + 2];
+
+			avgColor = (redColor + greenColor + blueColor) / 3;
+
+			for(int i = 0; i < 3; ++i)
+				vout[currentIndex + i] = avgColor;
+		}
+	}
 }
