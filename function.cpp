0739ca7d4f1c2a5d48e8f37f0ea8460a25ccf691
Stefan-Mada <Stefan.Mada@utah.edu>
2021-10-16 18:38:29 -0600

    Getting working up to date

--- function.cpp
+++ function.cpp
@@ -1,12 +1,14 @@
 #include <immintrin.h>
 
-__m256i function(__m256i vec1)
+__m256i function(__m256i vec1, __m256i vec2)
 {	
-	const __m256i vec2 = _mm256_setr_epi8(	1, 0, 3, 2, 5, 4, 7, 6, 5, 3, 27, 27, 25, 15, 13, 15,
-						16, 17, 18, 14, 13, 12, 31, 25, 16, 27, 1, 11, 12,
-					       	14, 15, 16);
-	vec1 = _mm256_shuffle_epi8(vec1, vec2);
-	vec1 = _mm256_shuffle_epi8(vec1, vec2);
-	
+	int32_t* vals = (int32_t*)&vec1;
+	int32_t* vals2 = (int32_t*)&vec2;
+	for(int i = 0; i < 8; ++i)
+		if(!i%2)
+			vals[i] -= vals2[i];
+		else
+			vals[i] += vals2[i];
 	return vec1;
+
 }
